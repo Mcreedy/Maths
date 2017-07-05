ee66ef8f3f36e110deef7015b29007d0ece98ad5
Mack Reed <mack@appliedgenerics.com>
2017-07-05 11:11:12 +0100

    AskForNumbers.py should work

--- AskForNumbers.py
+++ AskForNumbers.py
@@ -1,1 +1,12 @@
 #! usr/bin/env
+
+from HowManyNumbers import numofnum
+
+def whatNumbers():
+    repeatnumber = numberOfNumbers()
+    numbersToAdd = []
+
+    for loop in range (0,repeatnumber):
+        number = input(int("Enter Number", repeatnumber+1))
+        numbersToAdd.append(number)
+    return numbersToAdd
